a9ad5b8743ad41c11527e8b7511b277a864a996f
Sungmin Choi <sungmin.choi@lge.com>
2015-05-12 11:44:49 +0900

    jeter: Set drm.service.enabled to true
    Change-Id: Ifdcb62a96f2106c139df613b81fe12f068d37cec

--- properties.mk
+++ properties.mk
@@ -92,6 +92,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.cabl=0 \
     vendor.display.enable_default_color_mode=1
 
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.qfp=false
