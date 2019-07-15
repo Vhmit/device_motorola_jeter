9fedad7675e32d25abc35ad0b15eff3141a080eb
Yifan Hong <elsk@google.com>
2019-07-15 16:43:26 -0700

    jeter: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend
    Test: charger mode
    Bug: 124118169
    Change-Id: I3825d08e38d677bd7154879609857de302348b6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,7 +64,6 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 
 # Display
