89cb2bcffe14ba1953b740d8c0ac2fc4fc46b352
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-12-08 18:57:12 +0100

    jeter: Import relevant Android GO configs
    Change-Id: Ie6dc00094c6af8f561a5285daf74273a3e7d621d

--- device.mk
+++ device.mk
@@ -389,6 +389,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.jeter
