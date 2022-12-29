c47280c7287e31e4137c82aaa7ec248204684f3c
5H4ND <miqueiasantos20@gmail.com>
2022-12-29 18:27:30 -0300

    Revert "jeter: Initial ArrowOS bringup"
    This reverts commit d0a8db226857129218174c38ca6f18b3be20ea02.

--- lineage_jeter.mk
+++ lineage_jeter.mk
@@ -14,11 +14,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_jeter
+PRODUCT_NAME := lineage_jeter
 PRODUCT_DEVICE := jeter
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := Moto G6 Play
