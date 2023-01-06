58e0fae33847ec3b943fce9ef86a868b04942603
Vhmit <praxedes.9630@gmail.com>
2023-01-06 19:50:50 -0300

    crdroid bringup

--- lineage_jeter.mk
+++ lineage_jeter.mk
@@ -28,7 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jeter-user 9 PPPS29.118-57-5 d3be1 release-keys" \
     PRODUCT_NAME="jeter"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
+BUILD_FINGERPRINT := motorola/jeter/jeter:9/PPPS29.118-57-5/d3be1:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
