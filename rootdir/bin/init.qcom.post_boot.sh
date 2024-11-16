cb95898b0b921aa69b7ad820893e344b597096f6
Vhmit <viktor.9630@protonmail.com>
2024-11-16 19:31:36 -0300

    msm8937-common: rootdir: Allow CPU to run at 300mhz
    * The value is already set in the kernel mimir.
    
    Change-Id: I8a2d9b9b942714871e80711d67f74ddf32c82b7c
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -493,7 +493,7 @@ case "$target" in
                 else
                     8917_sched_dcvs_hmp
                 fi
-                echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+                echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
                 # re-enable thermal core_control now
                 echo 1 > /sys/module/msm_thermal/core_control/enabled
 
@@ -582,8 +582,8 @@ case "$target" in
                 else
                     8937_sched_dcvs_hmp
                 fi
-                echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-                echo 768000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+                echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+                echo 300000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
                 # Disable L2-GDHS low power modes
                 echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-gdhs/idle_enabled
                 echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-gdhs/suspend_enabled
