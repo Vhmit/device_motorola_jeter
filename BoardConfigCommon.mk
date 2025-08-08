c2b4a7f47b8c20e36f6592070249fd64b1a6ff98
Albert I <kras@raphielgang.org>
2025-08-08 11:21:41 -0300

    msm8937-common: init: Update dalvik heap configuration
    * Use 4 GB configuration for 3/4 GB devices
    * Use 2 GB configuration for 2 GB device
    * Values taken from AOSP 10
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I50491ddb4865bf46bd87ff7de4ae880c81737883

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,10 @@ endif
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE   := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_msm8937
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8937
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := \
--- properties.mk
+++ properties.mk
@@ -67,15 +67,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_min_cpu=2 \
     ro.vendor.qti.core_ctl_max_cpu=4
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
-
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
