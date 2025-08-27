a65c0055215a3a283a94859d0e8cdb1688480032
Bruno Martins <bgcngm@gmail.com>
2025-08-27 10:42:04 -0300

    jeter: Remove unused jars and radio libs
    * merging and adjusting some things from the original commits for the device.
    
    * Ref: https://github.com/moto-msm89xx/android_device_motorola_msm8937-common/commit/6bde8c594e27fd6298cf21c1e4921f7f1ecff1d4
           https://github.com/moto-msm89xx/android_device_motorola_msm8937-common/commit/43e2e89f1ef2af556a41b96eaae470c38c8c64d5
    
    Change-Id: I8bd9e66e5cc90c6ac6a26767d62e4b3aaf5665e9
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,10 +55,6 @@ fi
 # Load wrapped shim
 function blob_fixup() {
     case "${1}" in
-    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-        ;;
-
     # memset shim
     vendor/bin/charge_only_mode)
         for LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
